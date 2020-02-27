7b31b6ecbe1686a9d1e78f3604e324abcb404a98
Andrei Birzan <andrei_birzan@yahoo.com>
2020-02-27 19:30:42 +0200

    added tables to init script

--- mysql/init.sql
+++ mysql/init.sql
@@ -1,1 +1,31 @@
-CREATE DATABSE
+CREATE DATABASE db;
+
+USE db;
+
+CREATE TABLE QUESTION_POOL (
+    QuestionID integer not null primary key auto_increment,
+    Answer1 varchar(128) not null,
+    Answer2 varchar(128) not null,
+    Views integer default 0
+);
+
+CREATE TABLE FLAGGED_OFFENSIVE_QUESTIONS (
+    QuestionID integer not null primary key auto_increment,
+    Answer1 varchar(128) not null,
+    Answer2 varchar(128) not null,
+    Views integer default 0
+);
+
+CREATE TABLE FLAGGED_LOW_SCORE_QUESTIONS (
+    QuestionID integer not null primary key auto_increment,
+    Answer1 varchar(128) not null,
+    Answer2 varchar(128) not null,
+    Views integer default 0
+);
+
+CREATE TABLE USER_SUBMITTED_QUESTIONS (
+    QuestionID integer not null primary key auto_increment,
+    Answer1 varchar(128) not null,
+    Answer2 varchar(128) not null,
+    Views integer default 0
+);
