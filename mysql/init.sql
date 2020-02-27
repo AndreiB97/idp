e54e330f4288bfbe8833b6fead96737c7f545622
AndreiB97 <Mastermates90>
2020-02-27 20:00:16 +0200

    changed tables structure

--- mysql/init.sql
+++ mysql/init.sql
@@ -6,6 +6,9 @@ CREATE TABLE QUESTION_POOL (
     QuestionID integer not null primary key auto_increment,
     Answer1 varchar(128) not null,
     Answer2 varchar(128) not null,
+    Answer1_count integer default 0,
+    Answer2_count integer default 0,
+    Score integer default 0,
     Views integer default 0
 );
 
@@ -13,6 +16,9 @@ CREATE TABLE FLAGGED_OFFENSIVE_QUESTIONS (
     QuestionID integer not null primary key auto_increment,
     Answer1 varchar(128) not null,
     Answer2 varchar(128) not null,
+    Answer1_count integer default 0,
+    Answer2_count integer default 0,
+    Score integer default 0,
     Views integer default 0
 );
 
@@ -20,12 +26,14 @@ CREATE TABLE FLAGGED_LOW_SCORE_QUESTIONS (
     QuestionID integer not null primary key auto_increment,
     Answer1 varchar(128) not null,
     Answer2 varchar(128) not null,
+    Answer1_count integer default 0,
+    Answer2_count integer default 0,
+    Score integer default 0,
     Views integer default 0
 );
 
 CREATE TABLE USER_SUBMITTED_QUESTIONS (
     QuestionID integer not null primary key auto_increment,
     Answer1 varchar(128) not null,
-    Answer2 varchar(128) not null,
-    Views integer default 0
+    Answer2 varchar(128) not null
 );
