5113561e2657acb95d5bbf5674050a84cf340d57
AndreiB97 <Mastermates90>
2020-03-03 18:56:07 +0200

    added profanity checking for filter

--- filter/filter.py
+++ filter/filter.py
@@ -2,6 +2,7 @@ import mysql.connector
 import os
 import logging
 from time import sleep
+from profanity_check import predict_prob
 
 db = None
 retry_count = 0
@@ -19,7 +20,7 @@ def connect_to_db():
             database=os.environ['DB_NAME']
         )
 
-        logging.info('Connected to DB')
+        logger.info('Connected to DB')
     except mysql.connector.errors.InterfaceError:
         if retry_count < 5:
             retry_count -=- 1
@@ -33,10 +34,27 @@ def connect_to_db():
 
 def init_logger():
     global logger
-    logger = logging.getLogger(__name__)
+
+    logger = logging.getLogger('Filter')
     logger.setLevel(logging.INFO)
+
+
+def filter_user_submitted_questions():
+    cursor = db.cursor()
+    cursor.callproc('get_user_submitted_questions')
+
+    for result in cursor.stored_results():
+        for row in result.fetchall():
+            profanity_probabilities = predict_prob([row[1], row[2]])
+            for probability in profanity_probabilities:
+                if probability > 0.5:
+                    logger.info(f'Flagging {row} for profanity')
+                    cursor.callproc('flag_user_submitted_question', (row[0], ))
+                    db.commit()
+                    break
 
 
 if __name__ == '__main__':
     init_logger()
     connect_to_db()
+    filter_user_submitted_questions()
