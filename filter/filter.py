22fd009337f38be09f5b6e65cf219bbaf2d9f174
AndreiB97 <andrei_birzan@yahoo.com>
2020-04-02 18:10:14 +0300

    moved connection logic to separate file and changed scheduling logic

--- filter/filter.py
+++ filter/filter.py
@@ -1,51 +1,31 @@
-import mysql.connector
 import os
 import logging
+import schedule
 from time import sleep
 from profanity_check import predict_prob
+from db_connection.connect_helper import set_logger, connect_to_db
+
+# TODO catch mysql no procedure exceptions and retry
 
 db = None
-retry_count = 0
 logger = None
-
-
-def connect_to_db():
-    global db, retry_count
-
-    try:
-        db = mysql.connector.connect(
-            host=os.environ['DB_HOST'],
-            user=os.environ['DB_USER'],
-            passwd=os.environ['DB_PASS'],
-            database=os.environ['DB_NAME']
-        )
-
-        logger.info('Connected to DB')
-    except mysql.connector.errors.InterfaceError:
-        if retry_count < 5:
-            retry_count -=- 1
-            logger.warning(f'Unable to connect to DB. Retry #{retry_count}.')
-            sleep(1)
-            connect_to_db()
-        else:
-            logger.error('Maximum number of retries reached.')
-            raise
 
 
 def init_logger():
     global logger
 
-    logger = logging.getLogger('Filter')
+    logger = logging.Logger(name='Filter')
     logger.setLevel(logging.INFO)
 
 
-def filter_profanity(id, ans1, ans2, cursor):
+def filter_profanity(question_id, ans1, ans2, cursor):
     profanity_probabilities = predict_prob([ans1, ans2])
 
     for probability in profanity_probabilities:
         if probability > 0.5:
-            cursor.callproc('flag_user_submitted_question', (id, ))
+            cursor.callproc('flag_user_submitted_question', (question_id,))
             db.commit()
+
             return True
 
     return False
@@ -57,76 +37,73 @@ def scan_user_submitted_questions():
 
     for result in cursor.stored_results():
         for row in result.fetchall():
-            id = row[0]
+            question_id = row[0]
             ans1 = row[1]
             ans2 = row[2]
 
-            res = filter_profanity(id, ans1, ans2, cursor)
+            res = filter_profanity(question_id, ans1, ans2, cursor)
 
             if res is True:
                 logger.info(f'Flagging {row} for profanity')
 
 
 def get_rerun_timer():
-    timer = 0
+    timer_minutes_total = 0
 
-    rerun_seconds = os.environ.get('FILTER_RERUN_TIMER_SECONDS')
-    rerun_minutes = os.environ.get('FILTER_RERUN_TIMER_MINUTES')
-    rerun_hours = os.environ.get('FILTER_RERUN_TIMER_HOURS')
-    rerun_days = os.environ.get('FILTER_RERUN_TIMER_DAYS')
-
-    try:
-        if rerun_seconds is not None:
-            timer += int(rerun_seconds)
-    except TypeError:
-        logger.warning('Type of FILTER_RERUN_TIMER_SECONDS is not int')
-        logger.debug(f'FILTER_RERUN_TIMER_SECONDS={rerun_seconds}')
+    timer_minutes = os.environ.get('FILTER_RERUN_TIMER_MINUTES')
+    timer_hours = os.environ.get('FILTER_RERUN_TIMER_HOURS')
+    timer_days = os.environ.get('FILTER_RERUN_TIMER_DAYS')
 
     try:
-        if rerun_minutes is not None:
-            timer += int(rerun_minutes) * 60
+        if timer_minutes is not None:
+            timer_minutes_total += int(timer_minutes)
     except TypeError:
         logger.warning('Type of FILTER_RERUN_TIMER_MINUTES is not int')
-        logger.debug(f'FILTER_RERUN_TIMER_MINUTES={rerun_minutes}')
+        logger.debug(f'FILTER_RERUN_TIMER_MINUTES={timer_minutes}')
 
     try:
-        if rerun_hours is not None:
-            timer += int(rerun_hours) * 60 * 60
+        if timer_hours is not None:
+            timer_minutes_total += int(timer_hours) * 60
     except TypeError:
         logger.warning('Type of FILTER_RERUN_TIMER_HOURS is not int')
-        logger.debug(f'FILTER_RERUN_TIMER_HOURS={rerun_hours}')
+        logger.debug(f'FILTER_RERUN_TIMER_HOURS={timer_hours}')
 
     try:
-        if rerun_days is not None:
-            timer += int(rerun_days) * 24 * 60 * 60
+        if timer_days is not None:
+            timer_minutes_total += int(timer_days) * 24 * 60
     except TypeError:
         logger.warning('Type of FILTER_RERUN_TIMER_DAYS is not int')
-        logger.debug(f'FILTER_RERUN_TIMER_DAYS={rerun_days}')
+        logger.debug(f'FILTER_RERUN_TIMER_DAYS={timer_days}')
 
-    if timer == 0:
-        timer = 24 * 60 * 60
-        logger.debug(f'Using default timer value {timer}')
+    if timer_minutes_total == 0:
+        timer_minutes_total = 24 * 60
+        logger.debug('Using default timer value of 24 hours')
 
-    return timer
+    return timer_minutes_total
 
 
-def filter_low_score_question(id, views, score, cursor):
+def filter_low_score_question(question_id, views, score, cursor):
     if views > 100 and score < views * 0.10:
-        cursor.callproc('flag_low_score_question', (id, ))
+        cursor.callproc('flag_low_score_question', (question_id,))
         db.commit()
+
         return True
+
     return False
 
 
-def filter_priority(id, views, priority, cursor):
+def filter_priority(question_id, views, priority, cursor):
     if views < 100 and priority == 0:
-        cursor.callproc('give_priority', (id, ))
+        cursor.callproc('give_priority', (question_id,))
         db.commit()
+
         return True
     elif views >= 100 and priority == 1:
-        cursor.callproc('remove_priority', (id, ))
+        cursor.callproc('remove_priority', (question_id,))
         db.commit()
+
         return True
+
     return False
 
 
@@ -136,17 +113,18 @@ def scan_question_pool():
 
     for result in cursor.stored_results():
         for row in result.fetchall():
-            id = row[0]
+            question_id = row[0]
             score = row[5]
             views = row[6]
             priority = row[7]
 
-            res = filter_priority(id, views, priority, cursor)
+            res = filter_priority(question_id, views, priority, cursor)
 
             if res is True:
                 logger.info(f'Changed priority for {row}')
+                break
 
-            res = filter_low_score_question(id, views, score, cursor)
+            res = filter_low_score_question(question_id, views, score, cursor)
 
             if res is True:
                 logger.info(f'Flagging {row} for low score')
@@ -154,10 +132,13 @@ def scan_question_pool():
 
 if __name__ == '__main__':
     init_logger()
-    connect_to_db()
-    timer = get_rerun_timer()
+    set_logger(logger)
+    db = connect_to_db()
+    timer_minutes = get_rerun_timer()
+
+    schedule.every(timer_minutes).minutes.do(scan_question_pool)
+    schedule.every(timer_minutes).minutes.do(scan_user_submitted_questions)
 
     while True:
-        scan_user_submitted_questions()
-        scan_question_pool()
-        sleep(timer)
+        schedule.run_pending()
+        sleep(1)
